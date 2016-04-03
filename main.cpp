ea52c5df76b0d80dbdb508b6a1de1f61b2743db9
Valeriy <valeroon.men@gmail.com>
2016-04-03 16:49:31 +0300

    Window class

--- main.cpp
+++ main.cpp
@@ -20,14 +20,12 @@ int main( void )
     Mat frame;
     if ( ! capture.isOpened() )
     {
-        printf("Video Capturing not opened \n");
         return -1;
     }
     while ( capture.read(frame) )
     {
         if( frame.empty() )
         {
-            printf("No frames available !");
             break;
         }
         int c = waitKey(10);
@@ -44,14 +42,15 @@ void detectAndDisplay( Mat frame )
 {
     std::vector<Rect> faces;
     Mat frame_gray;
-    cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
-    equalizeHist( frame_gray, frame_gray );
+    cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+    equalizeHist(frame_gray, frame_gray);
     face_cascade.detectMultiScale( frame_gray, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(70, 70) );
     for( size_t i = 0; i < faces.size(); i++ )
     {
         Point center( faces[i].x + faces[i].width/2, faces[i].y + faces[i].height/2 );
         ellipse( frame, center, Size( faces[i].width/2, faces[i].height/2), 0, 0, 360, Scalar( 0, 13, 26), 4, 8, 0 );
     }
+    std::cout« "Face index" « i « ": x-" « faces[i].x + faces[i].width/2 « ", y-" « faces[i].y + faces[i].height/2 « endl;
     imshow( window_name, frame );
 }
 
